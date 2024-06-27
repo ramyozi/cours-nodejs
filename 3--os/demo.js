3e27c12e3c47d992eafa2a937b4f99cda990e87d
Ramzi <ramzibenmansour.pro@gmail.com>
2024-06-27 16:49:07 +0200

    enhance switch case

--- 3--os/demo.js
+++ 3--os/demo.js
@@ -1,8 +1,18 @@
 const os = require('os');
 const lr = require('../2/init-read-line');
 
+const map = new Map([
+    ['architecture', os.arch()],
+    ['os', os.type()],
+    ['cpus', os.cpus().length],
+    ['tempdir', os.tmpdir()],
+    ['memoiretotale', `${os.totalmem() / 1024 / 1024 / 1024} Go`],
+    ['memoirelibre', `${os.freemem() / 1024 / 1024 / 1024} Go`]
+]);
+
 
 function askWhatInfoHeWantThenDisplayIt( firstCall = true ) {
+
     const promptMessage = firstCall 
         ? 'Quelle information souhaitez-vous ? (\nLes choix : \n - tout.\n - architecture.\n - os.\n - cpus.\n - tempdir.\n - memoiretotale.\n - memoirelibre.\n - quit.\n - clear.\n - help.\n Mon choix: '
         : 'Quelle information souhaitez-vous ? \n Mon Choix: ';
@@ -22,24 +32,6 @@ function askWhatInfoHeWantThenDisplayIt( firstCall = true ) {
                 };
                 console.log(info);
                 break;
-            case 'architecture':
-                console.log('Architecture :', os.arch());
-                break;
-            case 'os':
-                console.log('Type de système d\'exploitation :', os.type());
-                break;
-            case 'cpus':
-                console.log('Nombre de CPUs :', os.cpus().length);
-                break;
-            case 'tempdir':
-                console.log('Répertoire temporaire :', os.tmpdir());
-                break;
-            case 'memoiretotale':
-                console.log('Mémoire totale :', `${os.totalmem() / 1024 / 1024 / 1024} Go`);
-                break;
-            case 'memoirelibre':
-                console.log('Mémoire libre :', `${os.freemem() / 1024 / 1024 / 1024} Go`);
-                break;
             case 'quit':
                 console.log('Fermeture...');
                 lr.close();
@@ -51,10 +43,14 @@ function askWhatInfoHeWantThenDisplayIt( firstCall = true ) {
                 console.log('Commandes disponibles : tout, architecture, os, cpus, tempdir, memoiretotale, memoirelibre, quit, clear, help');
                 break;
             default:
-                console.log('Cette information n\'est pas disponible. Tapez "help" pour voir les options.');
+                if (map.has(answer.toLowerCase())) {
+                    info[answer.toLowerCase()] = map.get(answer.toLowerCase());
+                    console.log(info);
+                } else {
+                    console.log('Cette information n\'est pas disponible. Tapez "help" pour voir les options.');
+                }
                 break;
         }
-
 
         if (answer.toLowerCase() !== 'quit') {
             askWhatInfoHeWantThenDisplayIt(false);  
