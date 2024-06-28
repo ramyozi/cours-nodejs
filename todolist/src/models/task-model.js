5b79a35fff7906ab9cf4f0109acf15f76389e639
Ramzi <ramzibenmansour.pro@gmail.com>
2024-06-28 17:20:37 +0200

    db task model schema

---
+++ todolist/src/models/task-model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: false
+    },
+    deadline: {
+        type: Date,
+        required: true
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: null
+    }
+});
+
+module.exports = mongoose.model('Task', taskSchema);
